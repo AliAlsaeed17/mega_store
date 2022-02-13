2fc0db37224189fd8350b6db34773215b46fff9d
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-13 02:32:54 +0200

    Add Google sign in

---
+++ lib/core/controllers/auth_controller.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthController extends GetxController {
+  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    super.onClose();
+  }
+
+  void googleSignIn() async {
+    final GoogleSignInAccount? googleAccount = await _googleSignIn.signIn();
+  }
+}
