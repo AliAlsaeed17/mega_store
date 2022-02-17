83068843fee07cb19e50ebc9c3943a436cec9626
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-17 23:15:28 +0200

    Add Register Screen

---
+++ lib/views/auth/register_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mega_store/core/controllers/auth_controller.dart';
+import 'package:mega_store/views/auth/login_screen.dart';
+import '../widgets/custom_button.dart';
+import '../widgets/custom_text.dart';
+import '../widgets/custom_text_form_field.dart';
+
+class RegisterScreen extends GetWidget<AuthController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Get.off(LoginScreen());
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50, right: 20, left: 20),
+          child: Column(
+            children: [
+              const CustomText(
+                text: "Sign Up",
+                fontSize: 30,
+              ),
+              const SizedBox(height: 10),
+              const SizedBox(height: 25),
+              CustomTextFormField(
+                text: "Full Name",
+                hintText: "Please enter your full name",
+                onSaved: (value) {},
+              ),
+              const SizedBox(height: 35),
+              CustomTextFormField(
+                text: "Email",
+                hintText: "Please enter your Email",
+                onSaved: (value) {},
+              ),
+              const SizedBox(height: 35),
+              CustomTextFormField(
+                text: "Password",
+                hintText: "********",
+                onSaved: (value) {},
+              ),
+              const SizedBox(height: 20),
+              CustomButton(text: "SIGN UP", onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
