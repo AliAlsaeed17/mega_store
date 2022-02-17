fe10218a3682b572556bf07c74fefcbda5cf1070
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-17 23:04:36 +0200

    Add ControlView

---
+++ lib/views/control_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mega_store/core/controllers/auth_controller.dart';
+import 'package:mega_store/views/auth/login_screen.dart';
+import 'package:mega_store/views/home_screen.dart';
+
+class ControlView extends GetWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return (Get.find<AuthController>().user != null)
+          ? const HomeScreen()
+          : LoginScreen();
+    });
+  }
+}
