764f0beb09f15b42b1ec769359ab01b27215be7a
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-19 10:30:29 +0200

    Refacroring bottom navigation bar

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -140,62 +140,6 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: GetBuilder<HomeController>(
-        init: Get.find<HomeController>(),
-        builder: (controller) => BottomNavigationBar(
-          elevation: 0,
-          selectedItemColor: Colors.black,
-          backgroundColor: Colors.grey.shade100,
-          items: [
-            BottomNavigationBarItem(
-              label: "",
-              activeIcon: Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text("Explore"),
-              ),
-              icon: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Image.asset(
-                  "assets/images/Icon_Explore.png",
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: "",
-              activeIcon: Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: Text("Cart"),
-              ),
-              icon: Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: Image.asset(
-                  "assets/images/Icon_Cart.png",
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: "",
-              activeIcon: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Text("Account"),
-              ),
-              icon: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Image.asset(
-                  "assets/images/Icon_User.png",
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-          ],
-          currentIndex: controller.navigatorBottomIndex,
-          onTap: (index) {
-            controller.updateNavigatorBottomIndex(index);
-          },
-        ),
-      ),
     );
   }
 }
---
+++ lib/views/widgets/cutom_bottom_navigation_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mega_store/core/controllers/home_controller.dart';
+
+class CutomBottomNavigationBar extends StatelessWidget {
+  const CutomBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeController>(
+      init: Get.find<HomeController>(),
+      builder: (controller) => BottomNavigationBar(
+        elevation: 0,
+        selectedItemColor: Colors.black,
+        backgroundColor: Colors.grey.shade100,
+        items: [
+          BottomNavigationBarItem(
+            label: "",
+            activeIcon: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text("Explore"),
+            ),
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Image.asset(
+                "assets/images/Icon_Explore.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            activeIcon: Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: Text("Cart"),
+            ),
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: Image.asset(
+                "assets/images/Icon_Cart.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            activeIcon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text("Account"),
+            ),
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Image.asset(
+                "assets/images/Icon_User.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
+        currentIndex: controller.navigatorBottomIndex,
+        onTap: (index) {
+          controller.updateNavigatorBottomIndex(index);
+        },
+      ),
+    );
+  }
+}
