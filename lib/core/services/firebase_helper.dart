f21decd960117cbc384cd585c9e5db3c15f221f2
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-21 14:38:38 +0200

    Add get categories from firebase

--- lib/core/services/firebase_helper.dart
+++ lib/core/services/firebase_helper.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mega_store/models/category_model.dart';
 import 'package:mega_store/models/user_model.dart';
 
 class FirebaseHelper {
   final CollectionReference _userCollectionRef =
       FirebaseFirestore.instance.collection("users");
+  List<CategoryModel> _categories = [];
 
   final CollectionReference _categoryCollectionRef =
       FirebaseFirestore.instance.collection("categories");
@@ -12,7 +14,13 @@ class FirebaseHelper {
     return await _userCollectionRef.doc(user.id).set(user.toJson());
   }
 
-  getCategoriesFromFirestore() async {
-    return await _categoryCollectionRef.get();
+  Future<List<CategoryModel>> getCategoriesFromFirestore() async {
+    await _categoryCollectionRef.get().then((snapshot) {
+      snapshot.docs.forEach((document) {
+        _categories.add(
+            CategoryModel.fromJson(document.data() as Map<String, dynamic>));
+      });
+    });
+    return _categories;
   }
 }
