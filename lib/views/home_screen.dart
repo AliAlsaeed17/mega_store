76ad9b21ba3572c6e198473198f5862ca8c506fe
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-22 01:34:23 +0200

    Update home screen to show best selling products

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -25,126 +25,135 @@ class HomeScreen extends StatelessWidget {
       builder: (controller) => controller.isLoading
           ? Center(child: CircularProgressIndicator())
           : Scaffold(
-              body: Container(
-                padding: const EdgeInsets.only(top: 55, left: 20, right: 20),
-                child: Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey.shade200,
-                      ),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.black,
+              body: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.only(top: 55, left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey.shade200,
+                        ),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.black,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 25),
-                    const CustomText(
-                      text: 'Categories',
-                      alignment: Alignment.centerLeft,
-                    ),
-                    const SizedBox(height: 15),
-                    Container(
-                      height: 120,
-                      child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: controller.categories.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: [
-                              Container(
-                                height: 50,
-                                width: 60,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.grey.shade100,
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8),
-                                  child: Image.network(
-                                      controller.categories[index].image!),
-                                ),
-                              ),
-                              const SizedBox(height: 20),
-                              CustomText(
-                                text: controller.categories[index].name!,
-                              ),
-                            ],
-                          );
-                        },
-                        separatorBuilder: (context, index) =>
-                            const SizedBox(width: 20),
+                      const SizedBox(height: 25),
+                      const CustomText(
+                        text: 'Categories',
+                        alignment: Alignment.centerLeft,
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        CustomText(
-                          text: "Best Selling",
-                          fontSize: 18,
-                        ),
-                        CustomText(
-                          text: "See All",
-                          fontSize: 16,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                      height: 300,
-                      child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: names.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width * .4,
-                            child: Column(
+                      const SizedBox(height: 15),
+                      Container(
+                        height: 120,
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: controller.categories.length,
+                          itemBuilder: (context, index) {
+                            return Column(
                               children: [
                                 Container(
+                                  height: 50,
+                                  width: 60,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.grey.shade100,
+                                  ),
                                   child: Padding(
                                     padding: const EdgeInsets.all(8),
-                                    child: Container(
-                                      height: 220,
-                                      width: MediaQuery.of(context).size.width *
-                                          .4,
-                                      child: Image.asset(
-                                        "assets/images/Image.png",
-                                        fit: BoxFit.fill,
+                                    child: Image.network(
+                                        controller.categories[index].image!),
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                CustomText(
+                                  text: controller.categories[index].name!,
+                                ),
+                              ],
+                            );
+                          },
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(width: 20),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          CustomText(
+                            text: "Best Selling",
+                            fontSize: 18,
+                          ),
+                          CustomText(
+                            text: "See All",
+                            fontSize: 16,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        height: 300,
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: controller.bestSellingProducts.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width * .4,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8),
+                                      child: Container(
+                                        height: 220,
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                .4,
+                                        child: Image.network(
+                                          controller.bestSellingProducts[index]
+                                              .image!,
+                                          fit: BoxFit.fill,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                const CustomText(
-                                  text: "BeoPlay Speaker",
-                                  alignment: Alignment.bottomCenter,
-                                ),
-                                const CustomText(
-                                  text: "Bang and Olison",
-                                  alignment: Alignment.bottomCenter,
-                                  color: Colors.grey,
-                                ),
-                                const SizedBox(height: 7),
-                                const CustomText(
-                                  text: "755\$",
-                                  alignment: Alignment.bottomCenter,
-                                  color: kPrimaryColor,
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                        separatorBuilder: (context, index) =>
-                            const SizedBox(width: 20),
+                                  CustomText(
+                                    text: controller
+                                        .bestSellingProducts[index].name!,
+                                    alignment: Alignment.bottomCenter,
+                                  ),
+                                  CustomText(
+                                    text: controller.bestSellingProducts[index]
+                                        .description!,
+                                    alignment: Alignment.bottomCenter,
+                                    color: Colors.grey,
+                                    maxLines: 1,
+                                  ),
+                                  const SizedBox(height: 7),
+                                  CustomText(
+                                    text: controller
+                                            .bestSellingProducts[index].price! +
+                                        "\$",
+                                    alignment: Alignment.bottomCenter,
+                                    color: kPrimaryColor,
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(width: 20),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
