d7020137de0edf0396589ed92ce06171ef2919e0
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-21 08:15:03 +0200

    Add home controller binding

--- lib/util/binding.dart
+++ lib/util/binding.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:mega_store/core/controllers/auth_controller.dart';
+import 'package:mega_store/core/controllers/home_controller.dart';
 import 'package:mega_store/core/controllers/navigation_controller.dart';
 
 class AppBinding extends Bindings {
@@ -7,5 +8,6 @@ class AppBinding extends Bindings {
   void dependencies() {
     Get.lazyPut(() => AuthController());
     Get.lazyPut(() => NavigationController());
+    Get.lazyPut(() => HomeController());
   }
 }
