44c0023b64a31de9e8e885782b463332c3518901
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-21 14:39:31 +0200

    Add loadding indicator

--- lib/core/controllers/home_controller.dart
+++ lib/core/controllers/home_controller.dart
@@ -1,3 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:mega_store/core/services/firebase_helper.dart';
+import 'package:mega_store/models/category_model.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  final FirebaseHelper _firebaseHelper = FirebaseHelper();
+  List<CategoryModel> _categories = [];
+  List<CategoryModel> get categories => _categories;
+  bool isLoading = false;
+
+  HomeController() {
+    getCategories();
+  }
+
+  void getCategories() async {
+    isLoading = true;
+    _categories = await _firebaseHelper.getCategoriesFromFirestore();
+    isLoading = false;
+    update();
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mega_store/constants.dart';
+import 'package:mega_store/core/controllers/home_controller.dart';
 import 'package:mega_store/views/widgets/custom_text.dart';
 import 'auth/login_screen.dart';
 
@@ -19,126 +20,134 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.only(top: 55, left: 20, right: 20),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.grey.shade200,
-              ),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 25),
-            const CustomText(
-              text: 'Categories',
-              alignment: Alignment.centerLeft,
-            ),
-            const SizedBox(height: 15),
-            Container(
-              height: 120,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: names.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      Container(
-                        height: 50,
-                        width: 60,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.grey.shade100,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child:
-                              Image.asset("assets/images/Icon_Mens Shoe.png"),
-                        ),
+    return GetBuilder<HomeController>(
+      init: Get.find<HomeController>(),
+      builder: (controller) => controller.isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Scaffold(
+              body: Container(
+                padding: const EdgeInsets.only(top: 55, left: 20, right: 20),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey.shade200,
                       ),
-                      const SizedBox(height: 20),
-                      CustomText(
-                        text: names[index],
-                      ),
-                    ],
-                  );
-                },
-                separatorBuilder: (context, index) => const SizedBox(width: 20),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                CustomText(
-                  text: "Best Selling",
-                  fontSize: 18,
-                ),
-                CustomText(
-                  text: "See All",
-                  fontSize: 16,
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Container(
-              height: 300,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: names.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width * .4,
-                    child: Column(
-                      children: [
-                        Container(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Container(
-                              height: 220,
-                              width: MediaQuery.of(context).size.width * .4,
-                              child: Image.asset(
-                                "assets/images/Image.png",
-                                fit: BoxFit.fill,
-                              ),
-                            ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.black,
                           ),
                         ),
-                        const CustomText(
-                          text: "BeoPlay Speaker",
-                          alignment: Alignment.bottomCenter,
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    const CustomText(
+                      text: 'Categories',
+                      alignment: Alignment.centerLeft,
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                      height: 120,
+                      child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: controller.categories.length,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.grey.shade100,
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Image.network(
+                                      controller.categories[index].image!),
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              CustomText(
+                                text: controller.categories[index].name!,
+                              ),
+                            ],
+                          );
+                        },
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(width: 20),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        CustomText(
+                          text: "Best Selling",
+                          fontSize: 18,
                         ),
-                        const CustomText(
-                          text: "Bang and Olison",
-                          alignment: Alignment.bottomCenter,
-                          color: Colors.grey,
-                        ),
-                        const SizedBox(height: 7),
-                        const CustomText(
-                          text: "755\$",
-                          alignment: Alignment.bottomCenter,
-                          color: kPrimaryColor,
+                        CustomText(
+                          text: "See All",
+                          fontSize: 16,
                         ),
                       ],
                     ),
-                  );
-                },
-                separatorBuilder: (context, index) => const SizedBox(width: 20),
+                    const SizedBox(height: 10),
+                    Container(
+                      height: 300,
+                      child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: names.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width * .4,
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Container(
+                                      height: 220,
+                                      width: MediaQuery.of(context).size.width *
+                                          .4,
+                                      child: Image.asset(
+                                        "assets/images/Image.png",
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const CustomText(
+                                  text: "BeoPlay Speaker",
+                                  alignment: Alignment.bottomCenter,
+                                ),
+                                const CustomText(
+                                  text: "Bang and Olison",
+                                  alignment: Alignment.bottomCenter,
+                                  color: Colors.grey,
+                                ),
+                                const SizedBox(height: 7),
+                                const CustomText(
+                                  text: "755\$",
+                                  alignment: Alignment.bottomCenter,
+                                  color: kPrimaryColor,
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(width: 20),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
