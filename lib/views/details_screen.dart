aa199bb0274e6c6282646c166582f96428fae1e9
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-23 17:01:39 +0200

    Update add to cat button functionality

--- lib/views/details_screen.dart
+++ lib/views/details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mega_store/constants.dart';
+import 'package:mega_store/core/controllers/cart_controller.dart';
+import 'package:mega_store/models/cart_product_model.dart';
 import 'package:mega_store/models/product_model.dart';
 import 'package:mega_store/views/widgets/custom_button.dart';
 import 'package:mega_store/views/widgets/custom_text.dart';
@@ -120,12 +123,19 @@ class DetailsScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                  Container(
-                    width: 150,
-                    height: 60,
-                    child: CustomButton(
-                      text: "Add to Cart",
-                      onPressed: () {},
+                  GetBuilder<CartController>(
+                    init: Get.find<CartController>(),
+                    builder: (controller) => Container(
+                      width: 150,
+                      height: 60,
+                      child: CustomButton(
+                        text: "Add to Cart",
+                        onPressed: () => controller.addProduct(CartProductModel(
+                            name: product.name,
+                            image: product.image,
+                            quantity: 1,
+                            price: product.price)),
+                      ),
                     ),
                   ),
                 ],
