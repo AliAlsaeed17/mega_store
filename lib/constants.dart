72064c554cda89f38cd2dd0b111fb04fd471edf1
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-23 16:40:32 +0200

    Add cart table name and its columns names constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromRGBO(0, 197, 105, 1);
+
+const String cartTableName = "CartTable";
+const String productNameColumn = "productName";
+const String productImageColumn = "productImage";
+const String productQuantityColumn = "productQuantity";
+const String productPriceColumn = "productPrice";
