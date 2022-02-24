217c388ef82e2169802fd153233ceb4efce46364
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-24 21:44:48 +0200

    Update cart table column names

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromRGBO(0, 197, 105, 1);
 
 const String cartTableName = "CartTable";
-const String productNameColumn = "productName";
-const String productImageColumn = "productImage";
-const String productQuantityColumn = "productQuantity";
-const String productPriceColumn = "productPrice";
+const String productNameColumn = "name";
+const String productImageColumn = "image";
+const String productQuantityColumn = "quantity";
+const String productPriceColumn = "price";
