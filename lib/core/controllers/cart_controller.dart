510004b9ba6634b83783044261a4ec5cd6a0e4b6
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-24 21:47:36 +0200

    Add get all products n cart

--- lib/core/controllers/cart_controller.dart
+++ lib/core/controllers/cart_controller.dart
@@ -6,13 +6,23 @@ class CartController extends GetxController {
   MegaStoreDatabaseHelper databaseHelper = MegaStoreDatabaseHelper.db;
   bool _loading = false;
   bool get loading => _loading;
+  List<CartProductModel> _products = [];
+  List<CartProductModel> get products => _products;
+
+  CartController() {
+    getAllProducts();
+  }
 
   void addProduct(CartProductModel cartProduct) async {
     await databaseHelper.insert(cartProduct);
     update();
   }
 
-  void sss() {
-    print("hellp");
+  getAllProducts() async {
+    _loading = true;
+    _products = await databaseHelper.getAllProducts();
+    print(products.length);
+    _loading = false;
+    update();
   }
 }
