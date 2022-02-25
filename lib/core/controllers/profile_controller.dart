839ef010909f64530a5d789a58dc5b5a7502a1f7
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-25 16:14:54 +0200

    Add get current user

--- lib/core/controllers/profile_controller.dart
+++ lib/core/controllers/profile_controller.dart
@@ -2,9 +2,26 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mega_store/core/services/local_storage_helper.dart';
+import 'package:mega_store/models/user_model.dart';
 
 class ProfileController extends GetxController {
   final LocalStorageHelper localStorageHelper = LocalStorageHelper();
+  late UserModel? _userModel;
+  UserModel? get userModel => _userModel;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getCurrentUser();
+  }
+
+  getCurrentUser() async {
+    await localStorageHelper.getUser.then((value) {
+      _userModel = value;
+    });
+    update();
+  }
+
   signOut() {
     GoogleSignIn().signOut();
     FirebaseAuth.instance.signOut();
