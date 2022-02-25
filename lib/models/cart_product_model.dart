83d179d36b4dab1dd4ff06a25bfa37a2522b8977
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-25 09:55:54 +0200

    Update cart product model

--- lib/models/cart_product_model.dart
+++ lib/models/cart_product_model.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CartProductModel {
-  String? name, image, price;
-  int? quantity;
+  String? productID, name, image, price;
+  int quantity = 1;
 
   CartProductModel({
+    required this.productID,
     required this.name,
     required this.image,
     required this.quantity,
@@ -12,14 +13,16 @@ class CartProductModel {
   });
 
   CartProductModel.fromJson(Map<dynamic, dynamic> map) {
+    productID = map['productID'];
     name = map['name'];
     price = map['price'];
     image = map['image'];
-    quantity = map['Quantity'];
+    quantity = map['quantity'];
   }
 
   toJson() {
     return {
+      'productID': productID,
       'name': name,
       'price': price,
       'image': image,
