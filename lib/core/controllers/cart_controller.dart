3c3371fdd5e5d53b596b6ab536cb26cb9a182c67
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-25 09:57:52 +0200

    Add get total price, increase and decrease functions

--- lib/core/controllers/cart_controller.dart
+++ lib/core/controllers/cart_controller.dart
@@ -8,21 +8,54 @@ class CartController extends GetxController {
   bool get loading => _loading;
   List<CartProductModel> _products = [];
   List<CartProductModel> get products => _products;
+  double _totalPrice = 0.0;
+  double get totalPrice => _totalPrice;
 
   CartController() {
     getAllProducts();
   }
 
-  void addProduct(CartProductModel cartProduct) async {
-    await databaseHelper.insert(cartProduct);
-    update();
-  }
-
   getAllProducts() async {
     _loading = true;
     _products = await databaseHelper.getAllProducts();
-    print(products.length);
     _loading = false;
+    getTotalPrice();
+  }
+
+  void addProduct(CartProductModel cartProduct) async {
+    if (products.isNotEmpty) {
+      for (var product in products) {
+        if (product.productID == cartProduct.productID) {
+          return;
+        }
+      }
+    }
+
+    await databaseHelper.insert(cartProduct);
+    print(cartProduct.quantity);
+    getAllProducts();
+  }
+
+  getTotalPrice() {
+    if (products.isNotEmpty) {
+      for (var product in products) {
+        _totalPrice += double.parse(product.price!) * product.quantity;
+      }
+    }
+    update();
+  }
+
+  increaseQuantity(int index) async {
+    _products[index].quantity++;
+    _totalPrice += double.parse(_products[index].price!);
+    await databaseHelper.update(_products[index]);
+    update();
+  }
+
+  decreaseQuantity(int index) async {
+    _products[index].quantity--;
+    _totalPrice -= double.parse(_products[index].price!);
+    await databaseHelper.update(_products[index]);
     update();
   }
 }
