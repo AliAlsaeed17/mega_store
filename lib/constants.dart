f9388b03767f995ec0407e67347beee795157ad8
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-25 09:54:49 +0200

    Add productID column name

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromRGBO(0, 197, 105, 1);
 
 const String cartTableName = "CartTable";
+const String productIDColumn = "productID";
 const String productNameColumn = "name";
 const String productImageColumn = "image";
 const String productQuantityColumn = "quantity";
