92301b5e8d528fc5d359201c1aca7c394242a6d3
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-25 16:15:23 +0200

    Update profile screen

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -10,18 +10,37 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<ProfileController>(
-      init: Get.find<ProfileController>(),
-      builder: (controller) => Container(
-        child: Center(
-          child: CustomButton(
-            text: 'Sign Out',
-            onPressed: () {
-              controller.signOut();
-              Get.offAll(LoginScreen());
-            },
-          ),
-        ),
-      ),
-    );
+        init: Get.find<ProfileController>(),
+        builder: (controller) => Container(
+              padding: EdgeInsets.only(top: 50),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 120,
+                            height: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(100),
+                              image: DecorationImage(
+                                image: controller.userModel!.pic == 'default'
+                                    ? AssetImage('assets/images/Image.png')
+                                        as ImageProvider
+                                    : NetworkImage(
+                                        controller.userModel!.pic as String),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ));
   }
 }
