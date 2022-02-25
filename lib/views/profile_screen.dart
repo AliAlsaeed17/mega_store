be0b218926112aec27ab1d433c34a16d5669e46d
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-25 11:01:34 +0200

    Update profile screen

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mega_store/core/controllers/profile_controller.dart';
+import 'package:mega_store/views/auth/login_screen.dart';
+import 'package:mega_store/views/widgets/custom_button.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GetBuilder<ProfileController>(
+      init: Get.find<ProfileController>(),
+      builder: (controller) => Container(
+        child: Center(
+          child: CustomButton(
+            text: 'Sign Out',
+            onPressed: () {
+              controller.signOut();
+              Get.offAll(LoginScreen());
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
