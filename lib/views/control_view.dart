c182cdb7cc295255a9abd8a0fed4602386289776
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-27 01:41:37 +0200

    Update control view with new navigation mechanism

--- lib/views/control_view.dart
+++ lib/views/control_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mega_store/core/controllers/auth_controller.dart';
+import 'package:mega_store/core/controllers/cart_controller.dart';
+import 'package:mega_store/core/controllers/home_controller.dart';
 import 'package:mega_store/core/controllers/navigation_controller.dart';
+import 'package:mega_store/core/controllers/profile_controller.dart';
 import 'package:mega_store/views/auth/login_screen.dart';
 import 'package:mega_store/views/home_screen.dart';
 import 'package:mega_store/views/widgets/cutom_bottom_navigation_bar.dart';
@@ -10,15 +13,19 @@ class ControlView extends GetWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      return (Get.find<AuthController>().userEmail == null)
+      return (!Get.find<AuthController>().completeAuthentication.value)
           ? LoginScreen()
           : GetBuilder<NavigationController>(
               init: Get.find<NavigationController>(),
-              builder: (controller) => Scaffold(
-                body: SafeArea(child: controller.currentScreen),
-                bottomNavigationBar: CustomBottomNavigationBar(),
-              ),
-            );
+              builder: (controller) {
+                Get.put(HomeController());
+                Get.put(CartController());
+                Get.put(ProfileController());
+                return Scaffold(
+                  body: SafeArea(child: controller.currentScreen),
+                  bottomNavigationBar: CustomBottomNavigationBar(),
+                );
+              });
     });
   }
 }
