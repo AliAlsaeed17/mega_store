87468c0523e4f0bf833a8a248c46ff39940fd3f8
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-27 01:39:03 +0200

    Update local storage helper

--- lib/core/services/local_storage_helper.dart
+++ lib/core/services/local_storage_helper.dart
@@ -6,24 +6,25 @@ import 'package:mega_store/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalStorageHelper {
-  Future<UserModel?> get getUser async {
+  Future<UserModel> get getUser async {
     try {
       UserModel user = await _getUserData();
       return user;
     } catch (e) {
       print(e.toString());
-      return null;
+      return UserModel(id: "0", email: "ERROR");
     }
   }
 
   setUser(UserModel user) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString(cachedUserData, json.encode(user.toJson()));
+    await prefs.setString(cachedUserData, json.encode(user.toJson()));
   }
 
   _getUserData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    return UserModel.fromJson(json.decode(prefs.get(cachedUserData) as String));
+    String userData = prefs.get(cachedUserData) as String;
+    return UserModel.fromJson(json.decode(userData));
   }
 
   deleteUser() async {
