8d507ad6aac809f7af68c2eee039198b308fc234
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-27 01:44:20 +0200

    Update profile screen

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:mega_store/core/controllers/profile_controller.dart';
 import 'package:mega_store/views/auth/login_screen.dart';
 import 'package:mega_store/views/widgets/custom_button.dart';
+import 'package:mega_store/views/widgets/custom_text.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -18,6 +19,7 @@ class ProfileScreen extends StatelessWidget {
                   children: [
                     Container(
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Container(
                             width: 120,
@@ -25,15 +27,35 @@ class ProfileScreen extends StatelessWidget {
                             decoration: BoxDecoration(
                               color: Colors.red,
                               borderRadius: BorderRadius.circular(100),
-                              image: DecorationImage(
-                                image: controller.userModel!.pic == 'default'
-                                    ? AssetImage('assets/images/Image.png')
-                                        as ImageProvider
-                                    : NetworkImage(
-                                        controller.userModel!.pic as String),
-                                fit: BoxFit.fill,
-                              ),
+                              // image: DecorationImage(
+                              //   image: controller.userModel!.pic == 'default'
+                              //       ? AssetImage('assets/images/Image.png')
+                              //           as ImageProvider
+                              //       : NetworkImage(
+                              //           controller.userModel!.pic as String),
+                              //   fit: BoxFit.fill,
+                              // ),
                             ),
+                            child: CustomButton(
+                              onPressed: () {
+                                controller.signOut();
+                              },
+                              text: "ssss",
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              CustomText(
+                                text: controller.userModel.fullName!,
+                                color: Colors.black,
+                                fontSize: 32,
+                              ),
+                              CustomText(
+                                text: controller.userModel.email!,
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ],
                           ),
                         ],
                       ),
