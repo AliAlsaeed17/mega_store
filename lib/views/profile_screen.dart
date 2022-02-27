bfc89e3ba54efeafadad2490e497fb3e8338eb9e
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-27 18:55:41 +0200

    Update profile screen

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -27,20 +27,14 @@ class ProfileScreen extends StatelessWidget {
                             decoration: BoxDecoration(
                               color: Colors.red,
                               borderRadius: BorderRadius.circular(100),
-                              // image: DecorationImage(
-                              //   image: controller.userModel!.pic == 'default'
-                              //       ? AssetImage('assets/images/Image.png')
-                              //           as ImageProvider
-                              //       : NetworkImage(
-                              //           controller.userModel!.pic as String),
-                              //   fit: BoxFit.fill,
-                              // ),
-                            ),
-                            child: CustomButton(
-                              onPressed: () {
-                                controller.signOut();
-                              },
-                              text: "ssss",
+                              image: DecorationImage(
+                                image: controller.userModel!.pic == 'default'
+                                    ? AssetImage('assets/images/Image.png')
+                                        as ImageProvider
+                                    : NetworkImage(
+                                        controller.userModel!.pic as String),
+                                fit: BoxFit.fill,
+                              ),
                             ),
                           ),
                           Column(
@@ -48,16 +42,133 @@ class ProfileScreen extends StatelessWidget {
                               CustomText(
                                 text: controller.userModel.fullName!,
                                 color: Colors.black,
-                                fontSize: 32,
+                                fontSize: 18,
                               ),
                               CustomText(
                                 text: controller.userModel.email!,
                                 color: Colors.black,
-                                fontSize: 18,
+                                fontSize: 14,
                               ),
                             ],
                           ),
                         ],
+                      ),
+                    ),
+                    SizedBox(height: 100),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: ListTile(
+                          title: CustomText(
+                            text: "Edit Profile",
+                            alignment: Alignment.centerLeft,
+                          ),
+                          leading: Icon(
+                            Icons.person,
+                          ),
+                          trailing: Icon(
+                            Icons.navigate_next,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: ListTile(
+                          title: CustomText(
+                            text: "Shipping Address",
+                            alignment: Alignment.centerLeft,
+                          ),
+                          leading: Icon(
+                            Icons.location_on,
+                          ),
+                          trailing: Icon(
+                            Icons.navigate_next,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: ListTile(
+                          title: CustomText(
+                            text: "Order History",
+                            alignment: Alignment.centerLeft,
+                          ),
+                          leading: Icon(
+                            Icons.history_rounded,
+                          ),
+                          trailing: Icon(
+                            Icons.navigate_next,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: ListTile(
+                          title: CustomText(
+                            text: "Cards",
+                            alignment: Alignment.centerLeft,
+                          ),
+                          leading: Icon(
+                            Icons.payment,
+                          ),
+                          trailing: Icon(
+                            Icons.navigate_next,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: ListTile(
+                          title: CustomText(
+                            text: "Notifications",
+                            alignment: Alignment.centerLeft,
+                          ),
+                          leading: Icon(
+                            Icons.notifications,
+                          ),
+                          trailing: Icon(
+                            Icons.navigate_next,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      child: GestureDetector(
+                        onTap: () {
+                          controller.signOut();
+                          Get.offAll(LoginScreen());
+                        },
+                        child: ListTile(
+                          title: CustomText(
+                            text: "Logout",
+                            alignment: Alignment.centerLeft,
+                          ),
+                          leading: Icon(
+                            Icons.logout,
+                          ),
+                          trailing: Icon(
+                            Icons.navigate_next,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
                     ),
                   ],
