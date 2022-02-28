e0b94f40f4351eb0352bd484138df404867e7f40
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-02-28 08:10:44 +0200

    Update profile screen

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -11,169 +11,170 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<ProfileController>(
-        init: Get.find<ProfileController>(),
-        builder: (controller) => Container(
-              padding: EdgeInsets.only(top: 50),
-              child: SingleChildScrollView(
-                child: Column(
+      init: Get.find<ProfileController>(),
+      builder: (controller) => Container(
+        padding: EdgeInsets.only(top: 50),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            width: 120,
-                            height: 120,
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(100),
-                              image: DecorationImage(
-                                image: controller.userModel!.pic == 'default'
-                                    ? AssetImage('assets/images/Image.png')
-                                        as ImageProvider
-                                    : NetworkImage(
-                                        controller.userModel!.pic as String),
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              CustomText(
-                                text: controller.userModel.fullName!,
-                                color: Colors.black,
-                                fontSize: 18,
-                              ),
-                              CustomText(
-                                text: controller.userModel.email!,
-                                color: Colors.black,
-                                fontSize: 14,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 100),
-                    Container(
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: ListTile(
-                          title: CustomText(
-                            text: "Edit Profile",
-                            alignment: Alignment.centerLeft,
-                          ),
-                          leading: Icon(
-                            Icons.person,
-                          ),
-                          trailing: Icon(
-                            Icons.navigate_next,
-                            color: Colors.black,
-                          ),
+                      width: 120,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(100),
+                        image: DecorationImage(
+                          image: controller.userModel!.pic == 'default'
+                              ? AssetImage('assets/images/Image.png')
+                                  as ImageProvider
+                              : NetworkImage(
+                                  controller.userModel!.pic as String),
+                          fit: BoxFit.fill,
                         ),
                       ),
                     ),
-                    SizedBox(height: 20),
-                    Container(
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: ListTile(
-                          title: CustomText(
-                            text: "Shipping Address",
-                            alignment: Alignment.centerLeft,
-                          ),
-                          leading: Icon(
-                            Icons.location_on,
-                          ),
-                          trailing: Icon(
-                            Icons.navigate_next,
-                            color: Colors.black,
-                          ),
+                    Column(
+                      children: [
+                        CustomText(
+                          text: controller.userModel.fullName!,
+                          color: Colors.black,
+                          fontSize: 18,
                         ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: ListTile(
-                          title: CustomText(
-                            text: "Order History",
-                            alignment: Alignment.centerLeft,
-                          ),
-                          leading: Icon(
-                            Icons.history_rounded,
-                          ),
-                          trailing: Icon(
-                            Icons.navigate_next,
-                            color: Colors.black,
-                          ),
+                        CustomText(
+                          text: controller.userModel.email!,
+                          color: Colors.black,
+                          fontSize: 14,
                         ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: ListTile(
-                          title: CustomText(
-                            text: "Cards",
-                            alignment: Alignment.centerLeft,
-                          ),
-                          leading: Icon(
-                            Icons.payment,
-                          ),
-                          trailing: Icon(
-                            Icons.navigate_next,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: ListTile(
-                          title: CustomText(
-                            text: "Notifications",
-                            alignment: Alignment.centerLeft,
-                          ),
-                          leading: Icon(
-                            Icons.notifications,
-                          ),
-                          trailing: Icon(
-                            Icons.navigate_next,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      child: GestureDetector(
-                        onTap: () {
-                          controller.signOut();
-                          Get.offAll(LoginScreen());
-                        },
-                        child: ListTile(
-                          title: CustomText(
-                            text: "Logout",
-                            alignment: Alignment.centerLeft,
-                          ),
-                          leading: Icon(
-                            Icons.logout,
-                          ),
-                          trailing: Icon(
-                            Icons.navigate_next,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
+                      ],
                     ),
                   ],
                 ),
               ),
-            ));
+              SizedBox(height: 100),
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ListTile(
+                    title: CustomText(
+                      text: "Edit Profile",
+                      alignment: Alignment.centerLeft,
+                    ),
+                    leading: Icon(
+                      Icons.person,
+                    ),
+                    trailing: Icon(
+                      Icons.navigate_next,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ListTile(
+                    title: CustomText(
+                      text: "Shipping Address",
+                      alignment: Alignment.centerLeft,
+                    ),
+                    leading: Icon(
+                      Icons.location_on,
+                    ),
+                    trailing: Icon(
+                      Icons.navigate_next,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ListTile(
+                    title: CustomText(
+                      text: "Order History",
+                      alignment: Alignment.centerLeft,
+                    ),
+                    leading: Icon(
+                      Icons.history_rounded,
+                    ),
+                    trailing: Icon(
+                      Icons.navigate_next,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ListTile(
+                    title: CustomText(
+                      text: "Cards",
+                      alignment: Alignment.centerLeft,
+                    ),
+                    leading: Icon(
+                      Icons.payment,
+                    ),
+                    trailing: Icon(
+                      Icons.navigate_next,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: ListTile(
+                    title: CustomText(
+                      text: "Notifications",
+                      alignment: Alignment.centerLeft,
+                    ),
+                    leading: Icon(
+                      Icons.notifications,
+                    ),
+                    trailing: Icon(
+                      Icons.navigate_next,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: GestureDetector(
+                  onTap: () {
+                    controller.signOut();
+                    Get.offAll(LoginScreen());
+                  },
+                  child: ListTile(
+                    title: CustomText(
+                      text: "Logout",
+                      alignment: Alignment.centerLeft,
+                    ),
+                    leading: Icon(
+                      Icons.logout,
+                    ),
+                    trailing: Icon(
+                      Icons.navigate_next,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
