14d32dfb04de8d49a546f745b839ca09036f6c8d
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-03-21 19:15:44 +0200

    Update constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,3 +10,25 @@ const String productQuantityColumn = "quantity";
 const String productPriceColumn = "price";
 
 const cachedUserData = 'chchedUserData';
+
+const kTileHeight = 50.0;
+const inProgressColor = Colors.grey;
+const todoColor = Color(0xffd1d2d7);
+
+const checkoutProcess = [
+  {'order': 0, 'step': 'delevery'},
+  {'order': 1, 'step': 'add address'},
+  {'order': 2, 'step': 'summary'},
+];
+
+enum CheckoutPages {
+  DeliveryTime,
+  AddAddress,
+  Summary,
+}
+
+enum DeliveryModes {
+  StandardDelivey,
+  NextDayDelivey,
+  NominatedDelivey,
+}
